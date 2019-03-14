ce838b6f652ded856317bc670b2ea300f5bc8390
Mirko Montabone <mirko.montabone@digital-lab.cl>
2019-03-14 12:56:59 -0300

    Get HTTP api spotify funcionando

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
-
-  paises: any[] = [];
+export class HomeComponent  {
 
 
-  constructor( private http: HttpClient ) {
-  
-    this.http.get('https://restcountries.eu/rest/v2/lang/es')
-      .subscribe( (resp: any)=> {
-        this.paises = resp;
-        console.log(resp);
-      } );
 
-   }
-
-  ngOnInit() {
+  constructor( private spotify: SpotifyService ){
+      this.spotify.getNewReleases();
   }
 
+  
 }
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log("Servicio Spotify listo");
+  }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBnI8TWKQ-Kxw14AqXkZoBOcPG-QLhjLopNmk8VuCSIJR8ToH9BVQOWuFtQY1-uXPcbxIuNOll6QdV6gxs'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+             .subscribe( data => {
+               console.log( data );
+             } )
+  }
+
+}
