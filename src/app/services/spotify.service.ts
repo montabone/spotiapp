ce2fef606888ca773947753a8ca0f5ddebac7860
Mirko Montabone <mirko.montabone@digital-lab.cl>
2019-03-15 13:27:52 -0300

    Centralizar peticiones hacia spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,26 +12,32 @@ export class SpotifyService {
     console.log("Servicio Spotify listo");
   }
 
-  getNewReleases(){
+  getQuery( query: string ){
+
+    const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQDvnNPiupWBC7YltH4yl8K1zrjItxW6Tl_5t6ew-Z7xeKDvxE7WuaWAobUEJzu70Iw9jMeQlzL6KcUIcuo'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-                    .pipe( map( data => data['albums'].items));
+    return this.http.get(url, {headers});
+
+  }
+
+
+  getNewReleases(){
+
+    return this.getQuery('browse/new-releases')
+               .pipe( map( data => data['albums'].items));                 
              
   }
 
 
   getArtista(termino:string){
     
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDvnNPiupWBC7YltH4yl8K1zrjItxW6Tl_5t6ew-Z7xeKDvxE7WuaWAobUEJzu70Iw9jMeQlzL6KcUIcuo'
-    });
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+               .pipe( map( data => data['artists'].items));
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
-                    .pipe( map( data=>data['artists'].items ));
 
   }
 
